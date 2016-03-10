677d8206a61fbe564f35d9989870bd626ecb3541
Aljaž Markežič <am9525@student.uni-lj.si>
2016-03-10 15:17:08 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -12,17 +12,26 @@ window.addEventListener('load', function() {
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
+			if(cas == 0){
+				alert("Opomnik!\n\nZadolžitev"+opomnik.value+" je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else{
+				cas -= 1;
+				casovnik.innerHTML= cas;
+			}
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
 	}
-	setInterval(posodobiOpomnike, 1000);
+	
 	document.getElementById("dodajGumb").addEventListener("click", function(){
 		var naziv = document.getElementById("naziv_opomnika").value;
 		var cas = document.getElementById("cas_opomnika").value;
 		document.getElementById("naziv_opomnika").value="";
 		document.getElementById("cas_opomnika").value="";
-		document.getElementById("opomniki").innerHTML+="<div class='opomnik'> <div class='naziv_opomnika'>"+naziv+"</div> <div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
+		document.getElementById("opomniki").innerHTML+="<div class='opomnik rob senca'> <div class='naziv_opomnika'>"+naziv+"</div> <div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
 		console.log(cas+" : "+naziv);
+		setInterval(posodobiOpomnike, 1000);
 	});
 	document.getElementById("prijavniGumb").addEventListener("click", function(){
 		var ime = document.getElementById("uporabnisko_ime").value;
